82dad4c6986e4827ba30e585fd9ad37c65abbb29
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-01 00:20:13 -0600

    fix: adding chart

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import Arrow from './Arrow'
 import Calendar from './Calendar'
+import Chart from './Chart'
+import Guests from './Guests'
+import Hamburger from './Hamburger'
+import Logo from './Logo'
+import Logout from './Logout'
+import Money from './Money'
+import Moon from './Moon'
+import NoData from './NoData'
+import Reports from './Reports'
+import Settings from './Settings'
+import Phone from './Phone'
+import Sun from './Sun'
+import User from './User'
+import X from './X'
 
 const SVG = {
   Arrow,
-  Calendar
+  Calendar,
+  Chart,
+  Guests,
+  Hamburger,
+  Logo,
+  Logout,
+  Money,
+  Moon,
+  NoData,
+  Phone,
+  Reports,
+  Settings,
+  Sun,
+  User,
+  X
 }
 
 export default SVG
