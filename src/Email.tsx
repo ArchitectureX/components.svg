83f99edbbf9137b1b46b2e6ab0b8d5dad8e7120d
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-05-28 23:58:35 -0700

    fix: Update email and lock

--- src/Email.tsx
+++ src/Email.tsx
@@ -3,25 +3,16 @@ import React, { FC } from 'react'
 type Props = {
   label?: string
   onClick?: any
-  stroke?: string
+  fill?: string
 }
 
-const SVG: FC<Props> = ({ label = undefined, onClick = undefined, stroke = '#666' }) => (
+const SVG: FC<Props> = ({ label = undefined, onClick = undefined, fill = '#666' }) => (
   <div data-component="SVG.Email" onClick={onClick} title={label}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect x="0.75" y="0.5" width="19" height="13" stroke={stroke} />
-      <path d="M4.25 4L10.25 9L16.25 4" stroke={stroke} strokeLinecap="square" />
-      <path d="M7.25 7L4.25 10" stroke={stroke} strokeLinecap="square" />
-      <path d="M16.25 10L13.25 7" stroke={stroke} strokeLinecap="square" />
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 1920 1920" fill={fill}>
+      <path
+        d="M1920 428.266v1189.54l-464.16-580.146-88.203 70.585 468.679 585.904H83.684l468.679-585.904-88.202-70.585L0 1617.805V428.265l959.944 832.441L1920 428.266ZM1919.932 226v52.627l-959.943 832.44L.045 278.628V226h1919.887Z"
+        fill-rule="evenodd"
+      />
     </svg>
   </div>
 )
--- src/Lock.tsx
+++ src/Lock.tsx
@@ -9,19 +9,23 @@ type Props = {
 const SVG: FC<Props> = ({ label = undefined, onClick = undefined, fill = '#666' }) => (
   <div data-component="SVG.Lock" onClick={onClick} title={label}>
     <svg
+      fill="#666"
+      width="24px"
+      height="24px"
+      version="1.1"
+      id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      viewBox="0 0 330 330"
     >
-      <path
-        d="M17 8H16V6C16 3.79 14.21 2 12 2C9.79 2 8 3.79 8 6V8H7C5.9 8 5 8.9 5 10V20C5 21.1 5.9 22 7 22H17C18.1 22 19 21.1 19 20V10C19 8.9 18.1 8 17 8ZM10 6C10 4.9 10.9 4 12 4C13.1 4 14 4.9 14 6V8H10V6ZM17 20H7V10H17V20ZM12 15C12.55 15 13 14.55 13 14C13 13.45 12.55 13 12 13C11.45 13 11 13.45 11 14C11 14.55 11.45 15 12 15Z"
-        fill={fill}
-      />
+      <g id="XMLID_509_">
+        <path
+          id="XMLID_510_"
+          d="M65,330h200c8.284,0,15-6.716,15-15V145c0-8.284-6.716-15-15-15h-15V85c0-46.869-38.131-85-85-85
+        S80,38.131,80,85v45H65c-8.284,0-15,6.716-15,15v170C50,323.284,56.716,330,65,330z M180,234.986V255c0,8.284-6.716,15-15,15
+        s-15-6.716-15-15v-20.014c-6.068-4.565-10-11.824-10-19.986c0-13.785,11.215-25,25-25s25,11.215,25,25
+        C190,223.162,186.068,230.421,180,234.986z M110,85c0-30.327,24.673-55,55-55s55,24.673,55,55v45H110V85z"
+        />
+      </g>
     </svg>
   </div>
 )
